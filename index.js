f5e1d6b163c24859e131b6bf18282e9b32f5157f
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-05-09 15:43:31 +0700

    rilis0 done 4 passing

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function create(data, callback){}
+const sqlite3 = require('sqlite3').verbose();
+const file = 'data.db';
+const db = new sqlite3.Database(file)
+const repl = require('repl');
+const REPLServer = repl.start({prompt : '> '});
 
-function read(callback){}
+function create(data, callback){
+  db.serialize(function(){
+    db.run(`INSERT INTO teacher(id,name,subject) VALUES ('${data.id}','${data.name}','${data.subject}');`, callback);
+  });
+}
 
-function update(callback){}
+function read(callback){
+  db.serialize(function(){
+    db.all('select * from teacher;',callback)
+  })
+}
 
-function deletes(callback){}
+function update(column_set,value_set,where_id,callback){
+  db.serialize(function(){
+    db.run(`UPDATE teacher SET '${column_set}' = '${value_set}'  WHERE id = '${where_id}';`, callback);
+  });
+}
 
+function deletes(id_deleted,callback){
+  db.serialize(function(){
+    db.run(`DELETE FROM teacher WHERE id = '${id_deleted}';`, callback);
+  });
+}
+
+let callbackData = (err,result) => {
+  if (err){
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+}
+
+REPLServer.context.create = create;
+REPLServer.context.read = read;
+REPLServer.context.update = update;
+REPLServer.context.deletes = deletes;
+REPLServer.context.callback = callbackData;
 module.exports = {
   create, read, update, deletes
 };
--- test/test.js
+++ test/test.js
@@ -6,22 +6,39 @@ const crud = require('../index.js')
  */
 describe('CREATE', function() {
   it('should invoke callback done', function(done) {
-    create({ id: 1, name: 'John Doe', subject: 'Foo Bar'}, done);
+    crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar'}, done);
   })
 })
 
+describe('READ', function() {
+  it('should invoke callback done', function(done) {
+    crud.read(done);
+  })
+})
+
+describe('UPDATE', function() {
+  it('should invoke callback done', function(done) {
+    crud.update('name','Not John Doe','1',done);
+  })
+})
+
+describe('DELETE', function() {
+  it('should invoke callback done', function(done) {
+    crud.deletes('1',done);
+  })
+})
 /**
  * contoh testing function dengan promise
  * comment apabila tidak digunakan
  */
-describe('CREATE promise', function() {
-  it('should resolve and invoke callback done', function(done) {
-    create()
-    .then(function() {
-      done()
-    })
-    .catch(function(err) {
-      done(err)
-    })
-  })
-})
+// describe('CREATE promise', function() {
+//   it('should resolve and invoke callback done', function(done) {
+//     create()
+//     .then(function() {
+//       done()
+//     })
+//     .catch(function(err) {
+//       done(err)
+//     })
+//   })
+// })
