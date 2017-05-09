df819d419dbeded8f0402f3af4af411bda52ce5d
Sidik Hidayatullah <sidik.riders@gmail.com>
2017-05-09 16:01:18 +0700

    release 2

--- index.js
+++ index.js
@@ -2,50 +2,134 @@
 
 const repl = require('repl');
 let replserver = repl.start({
-  prompt: '\(\~\'\,\'\)\~  ',
+  prompt: '\(\~\'v\'\)\~  ',
   input: process.stdin,
   output: process.stdout
 })
 const sqlite = require('sqlite3').verbose();
 const file = 'data.db';
 const db = new sqlite.Database(file);
+// function create(data, callback = createCB){
+//   let id = data[0];
+//   let name = data[1];
+//   let subject = data[2];
+//   let query = `INSERT INTO teacher ('id', 'name', 'subject') VALUES ('${id}', '${name}', '${subject}')`;
+//   db.serialize(function () {
+//     db.run(query, callback);
+//   })
+// }
 
-
-
-function create(data, callback = createCB){
+//PROMISE CREATE
+function create(data){
   let id = data[0];
   let name = data[1];
   let subject = data[2];
   let query = `INSERT INTO teacher ('id', 'name', 'subject') VALUES ('${id}', '${name}', '${subject}')`;
-  db.serialize(function () {
-    db.run(query, callback);
-  })
+  return new Promise(function (resolve, reject) {
+    db.serialize(function () {
+      db.run(query, function(err) {
+        if (err) reject(err)
+        else resolve()
+      });
+    });
+  });
 }
 
-function read(callback = readCB){
+function tambahData(data) {
+  create(data)
+  .then( () => {
+    console.log('INSERT berhasil!!')
+    showData();
+  })
+  .catch( err => console.log(err));
+}
+
+// function read(callback = readCB){
+//   let query = `SELECT * FROM teacher`;
+//   db.serialize(function () {
+//     db.all(query, callback);
+//   })
+// }
+
+//PROMISE READ
+function read(){
   let query = `SELECT * FROM teacher`;
-  db.serialize(function () {
-    db.all(query, callback);
-  })
+  return new Promise(function (resolve, reject) {
+    db.serialize(function () {
+      db.all(query, function(err, row) {
+        if (err) reject(err)
+        else resolve(row)
+      });
+    });
+  });
 }
+function showData() {
+  read()
+  .then(row => console.log(row))
+  .catch(err => console.log(err));
+}
+// function update(data, callback = updateCB){
+//   let id = data[0];
+//   let kategori = data[1];
+//   let databaru = data[2];
+//   let query = `UPDATE teacher SET ${kategori} = '${databaru}' WHERE id = '${id}'`;
+//   db.serialize(function() {
+//     db.run(query,callback)
+//   })
+// }
 
-function update(data, callback = updateCB){
+//PROMISE UPDATE
+function update(data){
   let id = data[0];
   let kategori = data[1];
   let databaru = data[2];
   let query = `UPDATE teacher SET ${kategori} = '${databaru}' WHERE id = '${id}'`;
-  db.serialize(function() {
-    db.run(query,callback)
-  })
+  return new Promise(function (resolve, reject) {
+    db.serialize(function() {
+      db.run(query, function(err) {
+        if (!err) resolve()
+        else reject(err);
+      });
+    });
+  });
 }
 
-function deletes(id, callback = deleteCB){
+function perbaharui(data) {
+  update(data)
+  .then(() => {
+    console.log('Update Berhasil')
+    showData()
+  })
+  .catch(err => console.log(err));
+}
+
+// function deletes(id, callback = deleteCB){
+//   let query = `DELETE FROM teacher WHERE id = ${id}`
+//   db.serialize(function() {
+//     db.run(query, callback);
+//   })
+// }
+
+//PROMISE DELETES
+function deletes(id){
   let query = `DELETE FROM teacher WHERE id = ${id}`
-  db.serialize(function() {
-    db.run(query, callback);
-  })
+  return new Promise(function (resolve, reject) {
+    db.serialize(function() {
+      db.run(query, function(err) {
+        if (!err) resolve()
+        else reject(err);
+      });
+    });
+  });
 }
 
+function hapus(id) {
+  deletes(id)
+  .then(() => {
+    console.log('Hapus berhasil!!')
+    showData();
+  }).catch(err => console.log(err));
+}
 let readCB = ((err, row) => {
   if (!err) {
     console.log('\n')
@@ -67,7 +151,7 @@ let createCB = (err => {
 let updateCB = (err => {
   if (!err) {
     console.log('\n' + 'update berhasil')
-    console.log(read())
+    showData()
   } else {
     console.log(err);
   }
@@ -76,7 +160,7 @@ let updateCB = (err => {
 let deleteCB = (err => {
   if (!err) {
     console.log(`\nHapus berhasil`)
-    console.log(read())
+    console.log(showData())
   } else {
     console.log('Error!!')
   }
@@ -84,7 +168,7 @@ let deleteCB = (err => {
 module.exports = {
   create, read, update, deletes
 };
-replserver.context.lihat = read;
-replserver.context.add = create;
-replserver.context.update = update;
-replserver.context.hapus = deletes;
+replserver.context.lihat = showData;
+replserver.context.add = tambahData;
+replserver.context.update = perbaharui;
+replserver.context.hapus = hapus;
--- test/test.js
+++ test/test.js
@@ -4,19 +4,19 @@ const crud = require('../index.js')
  * contoh testing function dengan callback
  * comment apabila tidak digunakan
  */
-describe('CREATE', function() {
-  it('should invoke callback done', function(done) {
-    crud.read(done);
-  })
-})
+// describe('CREATE', function() {
+//   it('should invoke callback done', function(done) {
+//     crud.read(done);
+//   })
+// })
 
 /**
  * contoh testing function dengan promise
  * comment apabila tidak digunakan
  */
-/*describe('CREATE promise', function() {
+describe('READ promise', function() {
   it('should resolve and invoke callback done', function(done) {
-    create()
+    crud.read()
     .then(function() {
       done()
     })
@@ -24,4 +24,40 @@ describe('CREATE', function() {
       done(err)
     })
   })
-})*/
+})
+
+describe('UPDATE promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    crud.update([ '2', 'subject', 'Lintas Budaya'])
+    .then(function() {
+      done()
+    })
+    .catch(function(err) {
+      done(err)
+    })
+  })
+})
+
+describe('INSERT promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    crud.create([ '104', 'Sodikin', 'Kebaran Lamoy'])
+    .then(function() {
+      done()
+    })
+    .catch(function(err) {
+      done(err)
+    })
+  })
+})
+
+describe('DELETE promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    crud.deletes(4)
+    .then(function() {
+      done()
+    })
+    .catch(function(err) {
+      done(err)
+    })
+  })
+})
