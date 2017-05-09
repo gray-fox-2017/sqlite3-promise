721ca2af0ecfde4c9f305297b1750bf3ded6f62c
ppsari <poppymighty@gmail.com>
2017-05-09 12:10:37 +0700

    p1w3d2_sqlite3WithPromise

--- index.js
+++ index.js
@@ -1,11 +1,111 @@
-function create(data, callback){}
+"use strict"
 
-function read(callback){}
+const repl = require('repl');
+const sqlite = require('sqlite3').verbose();
+let db = new sqlite.Database('data.db');
+let {createTable,seedData} = require('./setup.js');
 
-function update(callback){}
+let replServer = repl.start({
+  prompt: '>> ',
+  input: process.stdin,
+  output: process.stdout
+})
 
-function deletes(callback){}
+// function runQuery (query,hasReturn = false){
+//
+//   return new Promise((resolve,reject) => {
+//     db.serialize(() => {
+//       if (hasReturn) {
+//         db.all(query,(err,rows)=> {
+//           console.log('READ');
+//           return (err ?  reject(err) : resolve(rows));
+//         })
+//       } else {
+//         db.run(query, (err)=> {
+//           return (err ?  reject(err) : resolve());
+//         })
+//       }
+//     });
+//   });
+// }
+function create(data){
+  let query = `INSERT INTO teacher (name,subject) VALUES('${data.name}','${data.subject}')`;
 
+  return new Promise((resolve,reject) => {
+    db.serialize(() => {
+      db.run(query, (err)=> {
+        return (err ?  reject(err) : resolve());
+      })
+    });
+  });
+}
+
+function read(){
+  let query = `SELECT * FROM teacher`;
+  let callback = {printData : printData,printErr : printErr};
+
+  return new Promise((resolve,reject) => {
+    db.serialize(() => {
+      db.all(query,(err,rows)=> {
+        console.log('READ');
+        return (err ?  reject(err) : resolve(rows));
+      })
+
+    });
+  });
+  // runQuery(query,false)
+  // .then( (data) => printData(data) )
+  // .catch( (err) => printErr(err,'INSERT') );
+
+}
+
+function update(data){
+  let query = `UPDATE teacher SET name = '${data.name}', subject = '${data.subject}' WHERE id = '${data.id}'`
+  return new Promise((resolve,reject) => {
+    db.serialize(() => {
+      db.run(query, (err)=> {
+        return (err ?  reject(err) : resolve());
+      })
+    });
+  });
+  // runQuery(query,false)
+  // .then( () => printScs('UPDATE',data.id) )
+  // .catch( (err) => printErr(err,'UPDATE') );
+}
+
+function deletes(id){
+  let query = `DELETE FROM teacher where id = '${id}'`;
+  return new Promise((resolve,reject) => {
+    db.serialize(() => {
+      db.run(query, (err)=> {
+        return (err ?  reject(err) : resolve());
+      })
+    });
+  });
+  // runQuery(query,false,printErr)
+  // .then( () => printScs('DELETE',data.id) )
+  // .catch( (err) => printErr(err,'DELETE') );
+}
+
+const printData = (datas) => {
+  datas.forEach((x)=>{
+    console.log(`${x.id} | ${x.name} | ${x.subject}`);
+  });
+}
+const printScs = (activity,id) => {
+  console.log(`SUCCEED TO ${activity} ${id}`)
+}
+const printErr = (err,activity) => {
+  console.log(`FAILED TO ${activity}`)
+  console.log(err);
+}
 module.exports = {
-  create, read, update, deletes
+  create, read, update, deletes,printData,printErr,printScs
 };
+
+replServer.context.createTable = createTable;
+replServer.context.seedData = seedData;
+replServer.context.read = read;
+replServer.context.create = create;
+replServer.context.update = update;
+replServer.context.deletes = deletes;
--- test/test.js
+++ test/test.js
@@ -1,27 +1,78 @@
-const crud = require('../index.js')
-
+const {create, read, update, deletes,printData,printErr,printScs} = require('../index.js')
+const assert = require('assert');
 /**
  * contoh testing function dengan callback
  * comment apabila tidak digunakan
  */
-describe('CREATE', function() {
-  it('should invoke callback done', function(done) {
-    create({ id: 1, name: 'John Doe', subject: 'Foo Bar'}, done);
-  })
-})
+// describe('CREATE', function() {
+//   it('should invoke callback done', function(done) {
+//     create({name: 'John Doe', subject: 'Foo Bar'}, done());
+//   })
+// })
 
 /**
  * contoh testing function dengan promise
  * comment apabila tidak digunakan
- */
+  */
 describe('CREATE promise', function() {
   it('should resolve and invoke callback done', function(done) {
-    create()
-    .then(function() {
-      done()
+    create({name: 'Bob', subject: 'Lari dari kenyataan'})
+    .then(() => {
+      done();
     })
-    .catch(function(err) {
-      done(err)
+    .catch((err) => {
+      printErr(err,'CREATE');
+      done(err);
     })
-  })
-})
+
+
+  });
+});
+
+
+describe('READ promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    // crud.create({name: 'John Doe', subject: 'Foo Bar'})
+    read().
+    then((data) => {
+      printData(data);
+      done();
+    })
+    .catch((err) => {
+      printErr(err,'READ');
+      done(err);
+    })
+
+
+  });
+});
+
+describe('UPDATE promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    let id = 15;
+    update({name: 'Bukan JD', subject: 'Bukan FB', id: id })
+    .then(() => {
+      printScs('UPDATE',id)
+      done();
+    })
+    .catch((err) => {
+      printErr(err,'UPDATE');
+      done(err);
+    })
+  });
+});
+
+describe('DELETE promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    let id = 10;
+    deletes({id: id })
+    .then(() => {
+      printScs('DELETE',id)
+      done();
+    })
+    .catch((err) => {
+      printErr(err,'DELETE');
+      done(err);
+    })
+  });
+});
