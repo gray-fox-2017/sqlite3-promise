842d8a91a528cecf46a09c6e1fc5c91aa6a5a97c
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-05-09 14:54:52 +0700

    SQLite3 with Promise

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const crud = require('../index.js')
+const promise = require('../promise.js')
 
 /**
  * contoh testing function dengan callback
@@ -6,7 +7,22 @@ const crud = require('../index.js')
  */
 describe('CREATE', function() {
   it('should invoke callback done', function(done) {
-    create({ id: 1, name: 'John Doe', subject: 'Foo Bar'}, done);
+    crud.create({ id: 1, name: 'John Doe', subject: 'Foo Bar'}, done);
+  })
+})
+describe('READ', function() {
+  it('should invoke callback done', function(done) {
+    crud.read(done);
+  })
+})
+describe('UPDATE', function() {
+  it('should invoke callback done', function(done) {
+    crud.update(1, {name: 'Iam'}, done);
+  })
+})
+describe('DELETES', function() {
+  it('should invoke callback done', function(done) {
+    crud.deletes(1, done);
   })
 })
 
@@ -16,7 +32,40 @@ describe('CREATE', function() {
  */
 describe('CREATE promise', function() {
   it('should resolve and invoke callback done', function(done) {
-    create()
+    promise.create({ id: 1, name: 'John Doe', subject: 'Foo Bar'})
+    .then(function() {
+      done()
+    })
+    .catch(function(err) {
+      done(err)
+    })
+  })
+})
+describe('READ promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    promise.read()
+    .then(function() {
+      done()
+    })
+    .catch(function(err) {
+      done(err)
+    })
+  })
+})
+describe('UPDATE promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    promise.update(1, {name: 'Iam'})
+    .then(function() {
+      done()
+    })
+    .catch(function(err) {
+      done(err)
+    })
+  })
+})
+describe('DELETE promise', function() {
+  it('should resolve and invoke callback done', function(done) {
+    promise.deletes(1)
     .then(function() {
       done()
     })
