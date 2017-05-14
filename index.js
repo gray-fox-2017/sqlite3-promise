688d26c7a6d8a13fe635cb6ae1848cb0e9440169
ambodalle <ambodalle.st@gmail.com>
2017-05-14 23:04:42 +0700

    sqlite3 nvr

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
-function create(data, callback){}
+const file = 'data.db'
+const sqlite = require('sqlite3').verbose()
+const db = new sqlite.Database(file)
 
-function read(callback){}
 
-function update(callback){}
+function create(data){
+  return new Promise((resolve, reject) =>{
+    let CREATE = `INSERT INTO teacher (id, name,  subject) VALUES ('${data.id}', '${data.name}','${data.subject}')`
+    db.serialize(()=>{
+      db.run(CREATE, (err) =>{
+        if(!err) {
+          return resolve(data)
+        } else {
+          return reject (err)
+        }
+      })
+    })
+  })
+}
 
-function deletes(callback){}
+function read(){
+  return new new Promise((resolve, reject) => {
+    let SELECT = `SELECT * FROM teacher`
+    db.serialize(()=>{
+      db.all(SELECT, (err,rows) =>{
+        if(!err) {
+          return resolve(rows)
+        } else {
+          return reject(err)
+        }
+      })
+    })
+  });
+}
+
+function update(data){
+  return new Promise ((resolve, reject) =>{
+    let UPDATE = `UPDATE teacher SET '${data.column}' = '${data.value}' WHERE id = ${data.id} `
+    db.serialize(() =>{
+      db.run(UPDATE, (err) =>{
+        if(!err) {
+          return resolve (data)
+        } else {
+          return reject (err)
+        }
+      })
+    })
+  })
+}
+
+function deletes(data){
+  return new Promise((resolve, reject) =>{
+    let DELETE = `DELETE FROM teacher WHERE id = ${data.id}`
+    db.serialize(() =>{
+      db.run(DELETE, (err) =>{
+        if(!err) {
+          return resolve(data)
+        } else {
+          return reject(err)
+        }
+      })
+    })
+  })
+}
 
 module.exports = {
   create, read, update, deletes
