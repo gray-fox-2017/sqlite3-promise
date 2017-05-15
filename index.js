394fb3a4663bdbe45f8c1811bead6acb3c58eb60
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-05-15 14:04:19 +0700

    over deadline push

--- index.js
+++ index.js
@@ -4,43 +4,100 @@ const db = new sqlite3.Database(file)
 const repl = require('repl');
 const REPLServer = repl.start({prompt : '> '});
 
-function create(data, callback){
-  db.serialize(function(){
-    db.run(`INSERT INTO teacher(id,name,subject) VALUES ('${data.id}','${data.name}','${data.subject}');`, callback);
-  });
-}
+// function create(data, callback){
+//   db.serialize(function(){
+//     db.run(`INSERT INTO teacher(id,name,subject) VALUES ('${data.id}','${data.name}','${data.subject}');`, callback);
+//   });
+// }
+//
+// function read(callback){
+//   db.serialize(function(){
+//     db.all('select * from teacher;',callback)
+//   })
+// }
+//
+// function update(column_set,value_set,where_id,callback){
+//   db.serialize(function(){
+//     db.run(`UPDATE teacher SET '${column_set}' = '${value_set}'  WHERE id = '${where_id}';`, callback);
+//   });
+// }
+//
+// function deletes(id_deleted,callback){
+//   db.serialize(function(){
+//     db.run(`DELETE FROM teacher WHERE id = '${id_deleted}';`, callback);
+//   });
+// }
 
-function read(callback){
-  db.serialize(function(){
-    db.all('select * from teacher;',callback)
+// let callbackData = (err,result) => {
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log(result);
+//   }
+// }
+
+function create(data){
+  return new Promise((res,rej)=>{
+    db.serialize(() => {
+      db.run(`INSERT INTO teacher(id,name,subject) VALUES ('${data.id}','${data.name}','${data.subject}');`, (err) => {
+        if(!err){
+          res()
+        } else {
+          rej(err);
+        }
+      });
+    });
   })
 }
 
-function update(column_set,value_set,where_id,callback){
-  db.serialize(function(){
-    db.run(`UPDATE teacher SET '${column_set}' = '${value_set}'  WHERE id = '${where_id}';`, callback);
-  });
+function read(){
+  return new Promise((res,rej) => {
+    db.serialize(function(){
+      db.all('select * from teacher;', (err)=>{
+        if(!err){
+          res()
+        } else {
+          rej(err);
+        }
+      })
+    })
+  })
 }
 
-function deletes(id_deleted,callback){
-  db.serialize(function(){
-    db.run(`DELETE FROM teacher WHERE id = '${id_deleted}';`, callback);
-  });
+function update(column_set,value_set,where_id){
+  return new Promise((res,rej)=>{
+    db.serialize(() => {
+      db.run(`UPDATE teacher SET '${column_set}' = '${value_set}'  WHERE id = '${where_id}';`, (err) => {
+        if(!err){
+          res()
+        } else {
+          rej(err);
+        }
+      });
+    });
+  })
 }
 
-let callbackData = (err,result) => {
-  if (err){
-    console.log(err);
-  } else {
-    console.log(result);
-  }
+function deletes(id_deleted){
+  return new Promise((res,rej) => {
+    db.serialize(function(){
+      db.run(`DELETE FROM teacher WHERE id = '${id_deleted}';`, (err) => {
+        if (!err){
+          res()
+        } else {
+          rej(err)
+        }
+      });
+    });
+  })
 }
 
-REPLServer.context.create = create;
-REPLServer.context.read = read;
-REPLServer.context.update = update;
-REPLServer.context.deletes = deletes;
-REPLServer.context.callback = callbackData;
+
+// REPLServer.context.create = create;
+// REPLServer.context.read = read;
+// REPLServer.context.update = update;
+// REPLServer.context.deletes = deletes;
+// REPLServer.context.callback = callbackData;
 module.exports = {
   create, read, update, deletes
 };
